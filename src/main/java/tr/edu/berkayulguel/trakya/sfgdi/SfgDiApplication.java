a0ded8e39d4f5be134ae0ebd93203e9dcc4b0d8f
Berkay Ulguel <berkayyyulguel@gmail.com>
2021-06-18 00:45:26 +0300

    spring context

---
+++ src/main/java/tr/edu/berkayulguel/trakya/sfgdi/SfgDiApplication.java
@@ -0,0 +1,20 @@
+package tr.edu.berkayulguel.trakya.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tr.edu.berkayulguel.trakya.sfgdi.controllers.MyController;
+
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+    public static void main(String[] args) {
+
+        var context  = SpringApplication.run(SfgDiApplication.class, args);
+        MyController myController = (MyController) context.getBean("myController");
+
+        String greeting = myController.sayHello();
+        System.out.println(greeting);
+    }
+
+}
